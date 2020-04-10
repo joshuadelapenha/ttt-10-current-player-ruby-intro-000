64088b8dfe50a5d2793b9451fabfab699f666ccf
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-10 22:09:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,16 @@
 
+def turn_count(board)
+  counter = 0
+  board.each do |piece|
+  if piece == "X" || piece == "O"
+    counter += 1
+  end
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  elsif turn_count(board) % 3 == 0
+    "O"
+  end
+end
