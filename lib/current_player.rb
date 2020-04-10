7135957208bc9cb3b34ed42d602d7c30dd9370ac
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-10 22:19:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if turn_count(board).even
     "X"
-  elsif turn_count(board) % 3 == 0
+  elsif turn_count(board).odd
     "O"
   end
 end
